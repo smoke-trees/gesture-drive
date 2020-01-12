6b1b256c3a05bd3d304f5526022f57ae74337d8d
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-01-12 15:59:34 +0530

    Convert from .h5 to .protobuf

---
+++ Gesture Recognition/proto_coonverter.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 12 15:43:46 2020
+
+@author: Tanmay Thakur
+"""
+import tensorflow as tf
+
+from keras.models import load_model
+from keras import backend as K
+from tensorflow.python.framework.graph_util import convert_variables_to_constants
+
+
+"""
+In case you ran into the "incompatible with expected resource" issue with a model containing BatchNormization layers such as DenseNet, make sure to set the learning phase to 0 before loading the Keras model in a new session.
+"""
+K.set_learning_phase(0)
+
+model = load_model('base.model')
+
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        # Graph -> GraphDef ProtoBuf
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = convert_variables_to_constants(session, input_graph_def,
+                                                      output_names, freeze_var_names)
+        return frozen_graph
+
+
+frozen_graph = freeze_session(K.get_session(),
+                              output_names=[out.op.name for out in model.outputs])
+
+tf.train.write_graph(frozen_graph, "model", "tf_model.pb", as_text=False)
